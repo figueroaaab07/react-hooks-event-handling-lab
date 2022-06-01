69a3a943ab7a0515e539b41b87ef2490c6abe582
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-06-01 11:00:27 -0700

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function onBlurHandler() {
+  console.log('Hey! Eyes on me!');
+}
+
+function onFocusHandler() {
+  console.log('Good!');
+}
+
+function EyesOnMe() {
+  return (
+    <div>
+      <button onBlur={onBlurHandler} onFocus={onFocusHandler}>Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from 'react';
+
+function onChangeHandler() {
+  console.log('Entering password...');
+}
+function Keypad() {
+  return (
+    <div>
+      <input type="password" onChange={onChangeHandler} />
+    </div>
+  )
+}
+
+export default Keypad;
